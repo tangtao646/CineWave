195035d4f5578b557692331b59c5de5cdb0b4a95
tangtao <1730462119@qq.com>
2026-05-25 13:48:05 +0800

    修改桌面端应用图标

--- desktopApp/build.gradle.kts
+++ desktopApp/build.gradle.kts
@@ -32,8 +32,30 @@ compose.desktop {
 
         nativeDistributions {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
-            packageName = "com.example.kmp_demo"
+            packageName = "CineWave"
             packageVersion = "1.0.0"
+
+            description = "CineWave - 跨平台影音播放应用"
+            vendor = "CineWave"
+
+            // macOS 配置
+            macOS {
+                bundleID = "com.example.kmp_demo"
+                iconFile.set(project.file("src/main/resources/icons/icon.icns"))
+            }
+
+            // Windows 配置
+            windows {
+                menuGroup = "CineWave"
+                upgradeUuid = "c4e5f6a7-b8c9-4d0e-a1f2-3b4c5d6e7f80"
+                iconFile.set(project.file("src/main/resources/icons/icon.ico"))
+            }
+
+            // Linux 配置
+            linux {
+                packageName = "cinewave"
+                iconFile.set(project.file("src/main/resources/icons/icon.png"))
+            }
 
             appResourcesRootDir.set(layout.projectDirectory.dir("src/main/resources"))
         }
